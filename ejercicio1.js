922fb902d95c7c32cd154ec3d2576ac5f723267b
Joao Costa <miguel-cds@hotmail.com>
2024-06-27 00:22:12 +0200

    1ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,21 @@
+ /// *************************** Ejercicios *****
+
+  // Ejercio 1.
+
+
+  let miSuperheroeFavorito = "Hulk";
+
+  let numeroFavorito = 50;
+
+  let altura = 5;
+
+  let longitud = 10;
+
+  let suma = (altura + longitud);
+
+console.log(suma, miSuperheroeFavorito, numeroFavorito, altura, longitud, suma );
+// console.log(miSuperheroeFavorito);
+// console.log(numeroFavorito);
+// console.log(altura);
+// console.log(longitud);
+// console.log(suma);
