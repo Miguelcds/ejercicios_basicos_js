8c659a1c32ffd7538440d792d9c430b103ca1de1
Joao Costa <miguel-cds@hotmail.com>
2024-06-27 00:57:32 +0200

    sig

---
+++ ejercicio2.js
@@ -0,0 +1,13 @@
+// Ejercico 2.
+
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí...
+
+console.log( "Nombre:", jedi.nombre,  "Edad:", jedi.edad + 6 )
+
+let jedisAct = jedi;
+
+jedisAct = {nombre: "Luke Skywalker", edad: 25};
+
+console.log(jedisAct);
