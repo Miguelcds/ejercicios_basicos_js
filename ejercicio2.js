d51f743196286f65018b8a6777b41afa90a7f210
Joao Costa <miguel-cds@hotmail.com>
2024-06-27 19:10:18 +0200

    2.1

--- ejercicio2.js
+++ ejercicio2.js
@@ -11,3 +11,17 @@ let jedisAct = jedi;
 jedisAct = {nombre: "Luke Skywalker", edad: 25};
 
 console.log(jedisAct);
+
+
+// Ejercicio 2.1
+
+
+let name = "Leia";
+
+let firstName = " Organa";
+
+let age = 20 ;
+
+console.log ( "Soy "+ name + firstName + " y tengo " + age + " y soy una princesa de Alderaan")
+
+console.log ( "Soy",name,firstName, " y tengo ", age, "y soy una princesa de Alderaan")
