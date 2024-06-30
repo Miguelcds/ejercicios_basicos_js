52452ee99892a3f4052fdf92b33c692943ede042
Joao Costa <miguel-cds@hotmail.com>
2024-06-30 16:15:13 +0200

    6.3

---
+++ ejercicio6.js
@@ -0,0 +1,40 @@
+// EJERCICIO 6
+
+// 6.1
+
+let i = -1;
+
+while (i < 9) {
+  i++; 
+  console.log(i);
+}
+
+let numbers = 10
+
+
+for (let i = 0; i < numbers; i++) {
+  console.log(i); 
+}
+
+
+// 6.2 
+
+
+for (let i = 0; i < numbers ; i++) {
+    if ( i / 2 == 0 ){
+      console.log(i);
+    }
+  }
+
+
+// 6.3 
+
+for (let index = 0; index <= 10; index++) {
+    if (index < 10 ) {
+      console.log("Intentando dormir 🐑");
+    }
+    else {
+      console.log('¡Dormido!');
+    }
+    // console.log(index);
+  }
