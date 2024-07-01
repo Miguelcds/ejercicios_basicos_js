bef6c58858549e7ccabdbc2a19c0532b4861329e
Joao Costa <miguel-cds@hotmail.com>
2024-07-01 20:44:15 +0200

    9.

---
+++ ejercicio9.js
@@ -0,0 +1,14 @@
+// EJERCICIO 9
+
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+
+function sumNumbers(numberList) {
+  let suma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6];
+
+  return suma;
+   
+}
+
+console.log(sumNumbers(numbers));
