5ed7389d2c4c7317b7533b574fe4cc65696ffc4d
Joao Costa <miguel-cds@hotmail.com>
2024-07-04 17:01:50 +0200

    14

---
+++ ejercicio14.js
@@ -0,0 +1,27 @@
+// Ejercio 14
+
+const words = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+];
+
+function repeatCounter(list) {
+  const result = {};
+
+  list.forEach((value) => {
+    result[value] = (result[value] || 0) + 1;
+  });
+
+  console.log(result);
+}
+
+repeatCounter(words);
