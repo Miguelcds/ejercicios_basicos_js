f9971514eb217dd14d4089db90e64fa7b63fbe0e
Joao Costa <miguel-cds@hotmail.com>
2024-07-04 17:00:24 +0200

    13

---
+++ ejercicio13.js
@@ -0,0 +1,34 @@
+// Ejercicio 13
+
+
+const names = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  
+  
+  
+  function nameFinder(nameList, name) {
+    
+    const position = nameList.indexOf(name)
+    if (position === -1) {
+      console.log( false );
+    } else {
+      console.log(true, position);
+    }
+      
+    // const includes = nameList.includes(name)
+  
+  }
+  
+  
+  nameFinder(names, "Jessica")
