b7b43597c72e7345e350dd97d70e55c4cea61e45
Joao Costa <miguel-cds@hotmail.com>
2024-07-04 20:56:39 +0200

    20

---
+++ ejercicio20.js
@@ -0,0 +1,39 @@
+// Ejercicio 20
+
+
+const popularToys = []
+
+const toys = [
+    {id: 101, name: 'Super Soaker', sellCount: 15},
+    {id: 102, name: 'Tamagotchi', sellCount: 22},
+    {id: 103, name: 'Polly Pocket', sellCount: 8},
+    {id: 104, name: 'Yo-yo', sellCount: 33},
+    {id: 105, name: 'Pikachu plush toy', sellCount: 19},
+    {id: 106, name: 'Rubik\'s Cube', sellCount: 27},
+    {id: 107, name: 'Fidget Spinner', sellCount: 12},
+    {id: 108, name: 'Slinky', sellCount: 5},
+    {id: 109, name: 'Magic 8-Ball', sellCount: 38},
+    {id: 110, name: 'Troll Doll', sellCount: 20}
+];
+
+
+for (const item of toys) {
+  if (item.sellCount < 16) {
+    
+  } else {
+    popularToys.push(item)
+  }
+}
+
+console.log(popularToys);
+
+
+for (const item of toys) {
+    if (item.sellCount < 16) {
+      
+    } else {
+      popularToys.push(item.name)
+    }
+  }
+  
+  console.log("Los mas vendidos son : ", popularToys);
