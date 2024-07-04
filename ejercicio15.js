c7917a1c440c166d9b096fdadb71cb0fd10b45c3
Joao Costa <miguel-cds@hotmail.com>
2024-07-04 18:29:21 +0200

    15

---
+++ ejercicio15.js
@@ -0,0 +1,14 @@
+
+// Ejercicio 15
+
+const products = ['Camiseta de Metallica', 'Pantalón vaquero', 'Gorra de beisbol', 'Camiseta de Basket', 'Cinturón de Orión', 'AC/DC Camiseta']
+
+
+
+
+for (let index = 0; index < products.length; index++) {
+  if (products[index].includes("Camiseta")) {
+    console.log(products[index]);
+  }
+
+}
