e4f1234678dddf373e09a1df7b77392c7b0d66ab
Joao Costa <miguel-cds@hotmail.com>
2024-07-07 12:20:26 +0200

    2

--- ejercicio2.js
+++ ejercicio2.js
@@ -6,13 +6,6 @@ const jedi = {nombre: "Luke Skywalker", edad: 19};
 
 console.log( "Nombre:", jedi.nombre,  "Edad:", jedi.edad + 6 )
 
-let jedisAct = jedi;
-
-jedisAct = {nombre: "Luke Skywalker", edad: 25};
-
-console.log(jedisAct);
-
-
 // Ejercicio 2.1
 
 
