bbc7eb55459ccb6eb1dad266de28f498f3a52a1a
Joao Costa <miguel-cds@hotmail.com>
2024-07-07 12:35:55 +0200

    8.9

--- ejercicio8.js
+++ ejercicio8.js
@@ -3,7 +3,7 @@
 const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
 
 function findLongestWord(stringslist) {
-    let higher = stringslist[0]
+    let higher = stringslist[0];
     for (let i = 1; i < stringslist.length; i++) {
       if (stringslist[i].length > higher.length) {
         higher = stringslist[i]
@@ -11,5 +11,19 @@ function findLongestWord(stringslist) {
     }
     return higher
   }
-  
-  console.log(findLongestWord(avengers));
+
+  // console.log(findLongestWord(avengers));
+
+
+  function findLongestWord(stringslist){
+    let mayor = "";
+
+    for (const item of stringslist) {
+      if (item.length > mayor.length) {
+        mayor = item
+      }
+    }
+    console.log(mayor);
+  }
+
+  findLongestWord(avengers)
